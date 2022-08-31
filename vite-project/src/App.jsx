ef49f025cec10229757e8cbccf7cffee736ddfc4
mirnatetzner <mirnatetzner@gmail.com>
2022-08-30 23:47:01 -0300

    feat: :art: Arquitetura do projeto

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [countador, setCountador] = useState(0)
+
+  return (
+    <div className="App">
+    
+    </div>
+  )
+}
+
+export default App
