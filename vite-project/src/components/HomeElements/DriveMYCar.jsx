80a0077a5d4e0abbfea54d47a9e4610da3b08dd8
alicebranq <branq.alice@gmail.com>
2022-09-04 16:49:33 -0300

    feat: :art: Adição de fonte Cooper Hewitt na Home e no index.css

--- vite-project/src/components/HomeElements/DriveMYCar.jsx
+++ vite-project/src/components/HomeElements/DriveMYCar.jsx
@@ -7,10 +7,10 @@ export const DriveMyCar = () => {
             <div className="image-movie">
                 <img width={370} src="https://m.media-amazon.com/images/M/MV5BY2NkNDdmZTUtYWQ2MC00MjUyLTk1MGMtNWZiZjI3ZWMzNDYwXkEyXkFqcGdeQXVyNTY4ODAxODI@._V1_FMjpg_UX1000_.jpg"/>
             </div>
-            <div className="DMC-Description">
-                <h2> Drive My Car </h2>
+            <div className="DMC_Description">
+                <h2> DRIVE MY CAR </h2>
                 <h4> 2021 </h4>
-                <p> Dois anos após a morte de sua esposa, Yusuke Kafuku recebe uma oferta para dirigir uma peça num festival de teatro em Hiroshima. Lá, ele conhece Misaki, uma jovem reservada, designada para ser sua motorista. Enquanto passam tempo juntos, Kafuku confronta o mistério de sua esposa que o assombra.</p>
+                <p className="sinopse"> Dois anos após a morte de sua esposa, Yusuke Kafuku recebe uma oferta para dirigir uma peça num festival de teatro em Hiroshima. Lá, ele conhece Misaki, uma jovem reservada, designada para ser sua motorista. Enquanto passam tempo juntos, Kafuku confronta o mistério de sua esposa que o assombra.</p>
             </div>
         </div>
     )
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,14 +1,12 @@
-import { DriveMyCar } from "../components/FilmesAmostra/DriveMYCar"
+import { DriveMyCar } from "../components/HomeElements/DriveMYCar"
+import { Introduction } from "../components/HomeElements/Introduction"
 
 function Home() {
 
   return (
     <div className="Home">
-      <dir>
-        <h1> um titulo</h1>
-        <p>umans need to be close to other humans: when you say it like that, it sounds like your typical warm, fuzzy truism, the kind of platitude we all accept without question. The truth is that real closeness goes far beyond appreciation for—or adoration of—another person. It requires a fortitude that’s almost steely, an openness to self-examination that can be as painful as it is edifying. That’s one of the ideas at the heart of Ryûsuke Hamaguchi’s swimmingly gorgeous three-hour drama Drive My Car. The movie is tender like a rainstorm: only in the aftermath, after you’ve allowed time for its ideas to settle, does its full picture become clear. It’s the kind of movie that makes everything feel washed clean, a gentle nudge of encouragement suggesting that no matter how tired you feel, you can move on in the world.</p>
-      </dir>
 
+      <Introduction />
       <DriveMyCar />
     </div>
   )
