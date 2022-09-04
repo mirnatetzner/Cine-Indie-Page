12f41fa0bbcd86f03be8d32483a57c8ed9fb1094
alicebranq <branq.alice@gmail.com>
2022-09-04 13:39:42 -0300

    primeira tentativa de adição de CSS

---
+++ vite-project/src/components/FilmesAmostra/DriveMYCar.jsx
@@ -0,0 +1,17 @@
+import style from "./DMC.css"
+
+export const DriveMyCar = () => {
+
+    return (
+        <div className="DriveMyCar">
+            <div className="image-movie">
+                <img width={370} src="https://m.media-amazon.com/images/M/MV5BY2NkNDdmZTUtYWQ2MC00MjUyLTk1MGMtNWZiZjI3ZWMzNDYwXkEyXkFqcGdeQXVyNTY4ODAxODI@._V1_FMjpg_UX1000_.jpg"/>
+            </div>
+            <div className="DMC-Description">
+                <h2> Drive My Car </h2>
+                <h4> 2021 </h4>
+                <p> Dois anos após a morte de sua esposa, Yusuke Kafuku recebe uma oferta para dirigir uma peça num festival de teatro em Hiroshima. Lá, ele conhece Misaki, uma jovem reservada, designada para ser sua motorista. Enquanto passam tempo juntos, Kafuku confronta o mistério de sua esposa que o assombra.</p>
+            </div>
+        </div>
+    )
+}
