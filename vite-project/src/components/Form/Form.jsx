35ace7bf24db69ebbb9f96cd0193e342e6e5475d
joaofmds <ms.joao.felipe@gmail.com>
2022-09-05 21:10:45 -0300

    Estilização do formulário.

--- vite-project/src/components/Form/Form.jsx
+++ vite-project/src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'    
+import "./FormStyle.css"
 
 function Form() {
     function cadastrarUsuario(e) {
@@ -10,32 +11,40 @@ function Form() {
     const [email, setEmail] = useState()
 
     return (
-        <div>
-            <h1>Quer se tornar um parceiro da Kine Indie também?</h1>
-            <h2>Realize o seu cadastro e nossa equipe entrará em contato.</h2>
-            <form onSubmit={cadastrarUsuario}>
+        <div className="formPage">
+            <div className="description">
+            <h1 className="title">Quer se tornar um parceiro da Kine Indie também?</h1>
+            <h2 className="subtitle">Realize o seu cadastro e nossa equipe entrará em contato.</h2>
+            </div>
+            <form onSubmit={cadastrarUsuario} className="form">
                 <div>
-                    <label htmlFor="name">Nome:</label>
+                    <label htmlFor="name"><b>Nome:</b></label>
                     <input 
+                    className='input'
                     type="text" 
                     id="name" 
                     name="name" 
-                    placeholder="Digite o seu nome: "
+                    placeholder="Digite o seu nome"
                     onChange={(e) => {setName(e.target.value)}}
                     />
                 </div>
                 <div>
-                    <label htmlFor="email">Email:</label>
+                    <label htmlFor="email"><b>Email:</b></label>
                     <input 
+                    className='input'
                     type="email" 
                     id="email" 
                     name="email" 
-                    placeholder="Digite a sua senha: " 
+                    placeholder="Digite o seu email" 
                     onChange={(e) => {setEmail(e.target.value)}}
                     />
                 </div>
                 <div>
-                    <input type="submit" value="Cadastrar" />
+                    <input 
+                    className='button'
+                    type="submit" 
+                    value="Cadastrar" 
+                    />
                 </div>
             </form>
         </div>
