98f60fc3c2e930768ac3fc3347b99372e64884d5
mirnatetzner <mirnatetzner@gmail.com>
2022-09-09 15:55:07 -0300

    alterações no Admin.css e inclusão de gif na Admin.jsx

--- vite-project/src/pages/Admin/Admin.jsx
+++ vite-project/src/pages/Admin/Admin.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import gifAdmin from  '../../img/admin.gif'
 import './Admin.css'
 export const Admin = ()=>{
     return(
         <div className='corpoAdmin'>
-            <h1>Admin</h1>
+            <h1>Área do administrador</h1>
+            <img src={gifAdmin}/> 
             <div className="menu">
                 <div> <Link className='linknav' to='/lista_de_filmes' > Filmes Cadastrados </Link></div>
                 <div><Link className='linknav' to='/novo_filme' > Novo Filme </Link></div>
