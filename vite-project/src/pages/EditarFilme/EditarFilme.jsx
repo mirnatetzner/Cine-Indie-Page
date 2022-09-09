76e54c3294b1fbad355d5f46481b6f6ef0fbed07
Isaac Costa <isaac.systemilc@gmail.com>
2022-09-09 16:18:55 -0300

    estilizado página de edição dos filmes

--- vite-project/src/pages/EditarFilme/EditarFilme.jsx
+++ vite-project/src/pages/EditarFilme/EditarFilme.jsx
@@ -2,23 +2,21 @@ import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getFilmesByIdEditar, updateFilme } from "../../services/Api";
 import { useEffect } from "react";
 import { useState } from "react";
-import "./EditarFilme.module.css";
+import './EditarFilme.css'
 
 const EditarFilme = () => {
   const { id } = useParams();
   const [infoFilme, setInfoFilme] = useState([]);
   const [mostraFilme, setMostraFilme] = useState(true);
-  const [filmeAtualizado, setFilmeAtualizado] = useState(
-    {
-      nome: "",
-      genero: "",
-      duracao: "",
-      anoLancamento: "",
-      diretor: "",
-      sinopse: "",
-      image: ""
-    },
-  );
+  const [filmeAtualizado, setFilmeAtualizado] = useState({
+    nome: "",
+    genero: "",
+    duracao: "",
+    anoLancamento: "",
+    diretor: "",
+    sinopse: "",
+    image: "",
+  });
 
   useEffect(() => {
     getFilmesByIdEditar(id).then((response) => {
@@ -31,28 +29,26 @@ const EditarFilme = () => {
   const edit = () => {
     // event.preventDefault();
     console.log(filmeAtualizado);
-    updateFilme(id, filmeAtualizado)
-      .then((response) => {
-        alert("Filme Atualizado com sucesso!");
-        navigate("/lista_de_filmes");
-      })
-      
+    updateFilme(id, filmeAtualizado).then((response) => {
+      alert("Filme Atualizado com sucesso!");
+      navigate("/lista_de_filmes");
+    });
   };
 
-  
   return (
     <div className="editarContainer">
       {mostraFilme && (
-        <div>
-          <p>{infoFilme.id}</p>
-          <p>{infoFilme.nome}</p>
-          <p>{infoFilme.genero}</p>
-          <p>{infoFilme.duracao}</p>
-          <p>{infoFilme.anoLancamento}</p>
-          <p>{infoFilme.diretor}</p>
-          <p>{infoFilme.image}</p>
-
-          <button
+        <div className='containerMostrarFilme'>
+          <img src={infoFilme.image} />
+          <div className="mostrarFilmes">
+          <p><strong>Id:</strong> {infoFilme.id}</p>
+          <p><strong>Nome:</strong> {infoFilme.nome}</p>
+          <p><strong>Gênero:</strong> {infoFilme.genero}</p>
+          <p><strong>Duração:</strong> {infoFilme.duracao}</p>
+          <p><strong>Ano de Lançamento:</strong> {infoFilme.anoLancamento}</p>
+          <p><strong>Diretor:</strong> {infoFilme.diretor}</p>
+          </div>
+          <button className="editButton"
             onClick={(event) => {
               setMostraFilme(false);
             }}
@@ -62,84 +58,103 @@ const EditarFilme = () => {
         </div>
       )}
       {!mostraFilme && (
-        <div>
-          <label>Nome: </label>
-          <input
-            value={infoFilme.nome}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                nome: event.target.value,
-              });
-            }}
-          ></input>
-          <label>Gênero: </label>
-          <input
-            value={infoFilme.genero}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                genero: event.target.value,
-              });
-            }}
-          ></input>
-          <label>Duração: </label>
-          <input
-            value={infoFilme.duracao}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                duracao: event.target.value,
-              });
-            }}
-          ></input>
-          <label>Ano de Lançamento: </label>
-          <input
-            value={infoFilme.anoLancamento}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                anoLancamento: event.target.value,
-              });
-            }}
-          ></input>
-          <label>Diretor: </label>
-          <input
-           value={infoFilme.diretor}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                diretor: event.target.value,
-              });
-            }}
-          ></input>
-          <label>Sinopse: </label>
-          <input
-            value={infoFilme.sinopse}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                sinopse: event.target.value,
-              });
-            }}
-          ></input>
-          <label>Imagem: </label>
-          <input
-            value={infoFilme.image}
-            onChange={(event) => {
-              setFilmeAtualizado({
-                ...filmeAtualizado,
-                image: event.target.value,
-              });
-            }}
-          ></input>
-          <button type="button"
-            onClick={(event) => {
-              edit();
-            }}
-          >
-            Salvar
-          </button>
+        <div className="container-inputs">
+          <div className="alinhamentoInput">
+          <div className="displayColunm">
+            <label>Nome: </label>
+            <input
+              value={infoFilme.nome}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  nome: event.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <div className="displayColunm">
+            <label>Gênero: </label>
+            <input
+              value={infoFilme.genero}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  genero: event.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <div className="displayColunm">
+            <label>Duração: </label>
+            <input
+              value={infoFilme.duracao}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  duracao: event.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <div className="displayColunm">
+            <label>Ano de Lançamento: </label>
+            <input
+              value={infoFilme.anoLancamento}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  anoLancamento: event.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <div className="displayColunm">
+            <label>Diretor: </label>
+            <input
+              value={infoFilme.diretor}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  diretor: event.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <div className="displayColunm">
+            <label>Sinopse: </label>
+            <input
+              value={infoFilme.sinopse}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  sinopse: event.target.value,
+                });
+              }}
+            ></input>
+          </div>
+          <div className="displayColunm">
+            <label>Imagem: </label>
+            <input
+              value={infoFilme.image}
+              onChange={(event) => {
+                setFilmeAtualizado({
+                  ...filmeAtualizado,
+                  image: event.target.value,
+                });
+              }}
+            ></input>
+            </div>
+          </div>
+          
+            <button className="editButton"
+              type="button"
+              onClick={(event) => {
+                edit();
+              }}
+            >
+              Salvar
+            </button>
+        
         </div>
       )}
     </div>
