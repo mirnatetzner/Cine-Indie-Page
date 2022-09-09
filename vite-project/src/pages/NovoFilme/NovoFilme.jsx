d7d7ceb6b95968339e6d63d1632de06fbbeeaad2
mirnatetzner <mirnatetzner@gmail.com>
2022-09-09 17:55:28 -0300

    adiciona min-heidht e pequenas alterações em NovoFilme.css

--- vite-project/src/pages/NovoFilme/NovoFilme.jsx
+++ vite-project/src/pages/NovoFilme/NovoFilme.jsx
@@ -1,16 +1,16 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { FormFilmes } from '../../components/FormFilmes/FormFilmes'
 import { createFilme } from '../../services/Api'
-import styles from './NovoFilme.module.css'
+import './NovoFilme.css'
 
 export const CreateFilme = () => {
   const navigate = useNavigate()
   const params = useParams()
 
   return (
-    <div className={styles.createFilme}>
+    <div className="createFilme">
       <h1>{params.id ? "Editar Filme" : "Cadastrar Filme"}</h1>
-      <div className={styles.FormFilmes}>
+      <div className="FormFilmes">
         <FormFilmes hasUser={params.id} onFinish={(filme) => {
           createFilme(filme).then(response => {
             navigate('/filmes')
